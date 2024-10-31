538a920ad775ac7b8d78e5a0e68b9ebd8ec92d6e
Hau Nguyen Van <nguyenvanhauit1997@gmail.com>
2024-10-31 10:11:51 +0700

    add full route

--- src/pages/UniversalSwap/helpers.ts
+++ src/pages/UniversalSwap/helpers.ts
@@ -390,11 +390,11 @@ export const getProtocolsSmartRoute = (
 
 export const isAllowAlphaIbcWasm = (fromToken: TokenItemType, toToken: TokenItemType) => {
   // from chainId and to chainId is CELESTIA_CHAIN_ID & INJECTVE_CHAIN_ID
-  if ([toToken?.chainId, fromToken?.chainId].includes(COSMOS_CHAIN_ID_COMMON.INJECTVE_CHAIN_ID)) return true;
-  if ([toToken?.chainId, fromToken?.chainId].includes(COSMOS_CHAIN_ID_COMMON.CELESTIA_CHAIN_ID)) return true;
-  // cosmos -> cosmos
-  if (toToken?.cosmosBased && fromToken?.cosmosBased) return true;
-  return false;
+  // if ([toToken?.chainId, fromToken?.chainId].includes(COSMOS_CHAIN_ID_COMMON.INJECTVE_CHAIN_ID)) return true;
+  // if ([toToken?.chainId, fromToken?.chainId].includes(COSMOS_CHAIN_ID_COMMON.CELESTIA_CHAIN_ID)) return true;
+  // // cosmos -> cosmos
+  // if (toToken?.cosmosBased && fromToken?.cosmosBased) return true;
+  return true;
 };
 
 const toCoinGeckoIds = ['osmosis', 'cosmos', 'oraichain-token', 'usd-coin'];
@@ -425,6 +425,8 @@ const listAllowSmartRoute = {
  * @returns boolean
  */
 export const isAllowIBCWasm = (fromToken: TokenItemType, toToken: TokenItemType) => {
+  return false;
+
   const fromTokenIsOraichain = fromToken?.chainId === 'Oraichain';
   const fromTokenIsCosmos = fromToken?.cosmosBased;
 
