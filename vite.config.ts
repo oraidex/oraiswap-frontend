b9cdb8dea95ba348cd1e9dbb6c691addd96cae85
vuonghuuhung <vuonghuuhung2002@gmail.com>
2024-12-03 01:23:28 +0700

    feat: add target option to rollupOptions in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -36,6 +36,7 @@ export default defineConfig({
   build: {
     commonjsOptions: { transformMixedEsModules: true },
     outDir: path.resolve(__dirname, 'build'),
-    rollupOptions: {}
+    rollupOptions: {},
+    target: "esnext"
   }
 });
