9f7ce82e3a3d672df253b2bce26915d1d8d30dcb
trungbach <trunga2k29@gmail.com>
2024-12-19 17:39:43 +0700

    feat: update axios baseURL to staging environment and add TODO for production update

--- src/rest/request.ts
+++ src/rest/request.ts
@@ -11,7 +11,9 @@ const axios = Axios.create({
       threshold: AXIOS_THROTTLE_THRESHOLD
     })
   ),
-  baseURL: import.meta.env.VITE_APP_BASE_API_URL
+  // baseURL: import.meta.env.VITE_APP_BASE_API_URL
+  // TODO: Update to prod later
+  baseURL: 'https://api-staging.oraidex.io'
 });
 
 export default axios;
