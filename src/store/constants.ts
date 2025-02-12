4ebe6368325633ff6cd42af05c88bcf716c2f826
trungbach <trunga2k29@gmail.com>
2025-02-12 11:29:39 +0700

    chore: update persistence version to 3.2

--- src/store/constants.ts
+++ src/store/constants.ts
@@ -1,5 +1,5 @@
 // change version when update, add state redux-persist storage
-export const PERSIST_VER = 3.1;
+export const PERSIST_VER = 3.2;
 
 export const PERSIST_CONFIG_KEY = 'root';
 export const ADDRESS_BOOK_KEY_BACKUP = 'addressBookBackup';
