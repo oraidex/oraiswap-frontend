07a2d491c2a0902b0a5fd2f04f78ca09a051f42d
vuonghuuhung <vuonghuuhung2002@gmail.com>
2025-02-12 16:58:08 +0700

    fix: update COMMON URL and add error handling for token inspector initialization

--- src/initTokenInspector.ts
+++ src/initTokenInspector.ts
@@ -1,14 +1,20 @@
 import { OraichainInspector, SolanaInspector, TokenInspector } from '@oraichain/orai-token-inspector';
 
-const COMMON = 'https://oraicommon-staging.oraidex.io';
+const COMMON = 'https://oraicommon.oraidex.io';
 
 export const getTokenInspectorInstance = async () => {
   if (!window.tokenInspector) {
-    const [ibcRes, assetsRes] = await Promise.all([
-      fetch(`${COMMON}/api/v1/chain-registry/ibc`),
-      fetch(`${COMMON}/api/v1/chain-registry/assets`)
-    ]);
-    const [ibc, assets] = await Promise.all([ibcRes.json(), assetsRes.json()]);
+    let ibc = [];
+    let assets = [];
+    try {
+      const [ibcRes, assetsRes] = await Promise.all([
+        fetch(`${COMMON}/api/v1/chain-registry/ibc`),
+        fetch(`${COMMON}/api/v1/chain-registry/assets`)
+      ]);
+      [ibc, assets] = await Promise.all([ibcRes.json(), assetsRes.json()]);
+    } catch (error) {
+      console.log(error);
+    }
 
     const oraichainInspector = await OraichainInspector.create(
       'https://rpc.orai.io/',
