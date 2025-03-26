7bc30fad2f47d064eb3123884c4e222a7774aebf
Hau Nguyen Van <nguyenvanhauit1997@gmail.com>
2025-03-26 13:45:25 +0700

    hide co-havest

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,7 +7,7 @@ const Balance = lazy(() => import('pages/Balance'));
 const PoolsV3 = lazy(() => import('pages/Pool-V3'));
 const PoolDetail = lazy(() => import('pages/Pools/PoolDetail'));
 const UniversalSwap = lazy(() => import('pages/UniversalSwap/index'));
-const CoHarvest = lazy(() => import('pages/CoHarvest'));
+// const CoHarvest = lazy(() => import('pages/CoHarvest'));
 const BitcoinDashboardV2 = lazy(() => import('pages/BitcoinDashboardV2'));
 const StakingPage = lazy(() => import('pages/Staking'));
 const DownloadApp = lazy(() => import('pages/DownloadApp'));
@@ -42,7 +42,7 @@ export default () => (
       <Route path="/pools-v3" element={<Navigate to="/pools" replace />} />
       <Route path="/pools/v3/:poolId" element={<PoolV3Detail />} />
       <Route path="/pools/v2/:poolUrl" element={<PoolDetail />} />
-      <Route path="/co-harvest" element={<CoHarvest />} />
+      {/* <Route path="/co-harvest" element={<CoHarvest />} /> */}
       <Route path="/bitcoin-dashboard-v2" element={<BitcoinDashboardV2 />} />
       <Route path="/download-owallet" element={<DownloadApp />} />
       <Route path="/staking" element={<StakingPage />} />
