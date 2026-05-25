c440f6a7ef9c768d037d22a873dcfb8ab5eaeee2
Hau Nguyen Van <54303398+haunv3@users.noreply.github.com>
2026-05-25 16:00:34 +0700

    update rpc mainnet solana (#1214)
    * update rpc mainnet solana
    
    * update rpc mainnet solana

--- src/program/web3.ts
+++ src/program/web3.ts
@@ -34,11 +34,13 @@ export const ORAICHAIN_RELAYER_ADDRESS_AGENTS = 'orai1ym6qytsu7skv2flw89y0mkey4g
 export const ORAICHAIN_RELAYER_ADDRESS_DEFAI_MEME = 'orai1rrlmvsaukfeg874fjsuxntsl22hw2j6u65hyng';
 export const connection = 'https://solana-public.agents.land';
 export const connectionMainnet: string[] = [
-  'https://kora-8cwrc2-fast-mainnet.helius-rpc.com',
-  'https://api.mainnet.solana.com', // Official Solana Labs
-  // 'https://solana-rpc.publicnode.com', // PublicNode (Allnodes), free, high volume
-  'https://rpc.ankr.com/solana', // Ankr public RPC (rate-limited)
-  'https://api.mainnet-beta.solana.com' // Official alternative
+  // 'https://kora-8cwrc2-fast-mainnet.helius-rpc.com',
+  // 'https://api.mainnet.solana.com', // Official Solana Labs
+  'https://solana-mainnet.phantom.app/YBPpkkN4g91xDiAnTE9r0RcMkjg0sKUIWvAfoFVJ',
+  'https://rpc.ironforge.network/mainnet?apiKey=01HYNQ388BQNCFHTKG8WHRMMWC',
+  'https://solana-rpc.publicnode.com' // PublicNode (Allnodes), free, high volume
+  // 'https://rpc.ankr.com/solana', // Ankr public RPC (rate-limited)
+  // 'https://api.mainnet-beta.solana.com' // Official alternative
   // 'https://solana-mainnet.gateway.tatum.io/'
 ];
 
